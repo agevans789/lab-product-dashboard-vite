19eeb51420b59a60beab343990a5ed16389929a0
Ann Grace Evans <agevans789@gmail.com>
2025-11-08 15:52:44 -0500

    add bootstrap buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ProductList from './components/ProductList';
+import styles from './components/App.css';
 
 const App = () => {
 
@@ -11,32 +12,40 @@ const App = () => {
   ];
   
   // TODO: Implement state to manage filtering
-  const filterProducts = () = {
-    const 
+  const filterProducts = () => {
     const [filterStatus, setFilterStatus] = useState('all');
     
     // TODO: Implement logic to filter products based on availability
-  const filteredProducts = products.filter(product => {
-    if (filterStatus === 'inStock') {
-      return product.inStock === true
-    }
-  })
-  }
-  
+    const filteredProducts = products.filter(product => {
+      if (filterStatus === 'inStock') {
+        return product.inStock === true;
+      }
+      if (filterStatus === 'outOfStock') {
+        return product.inStock === false;
+      }
+      return true;
+    })
 
+  const showAll = () => setFilterStatus('all');
+  const showInStock = () => setFilterStatus('inStock');
+  const showOutOfStock = () => setFilterStatus('outOfStock');
   
-
   return (
     <div>
       <header id="header">
-        <h1>{dashboardTitle}</h1>
+        <h1>Product Dashboard</h1>
       </header>
       <main>
-        <button onClick={()=> filterAvailability()}>Filter By Availability</button>
+        <div className={btn-group}>
+          <button type={button} className={btn btn-primary} onClick={showAll}>Show All</button>
+          <button type={button} className={btn btn-secondary} onClick={showInStock}>Show In Stock</button>
+          <button type={button} className={btn btn-primary} onClick={showOutOfStock}>Show Out of Stock</button>
+        </div>
         <ProductList products={products}/>
       </main>
     </div>
   )
+};
 };
 
 export default App;
